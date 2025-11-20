ed6d2c82eddb17c659daaf69cea45430362750bb
urs <hugogreyesa@gmail.com>
2025-11-19 20:46:29 -0600

    sectors homogenization process

--- datalifecycle/Pf3.py
+++ datalifecycle/Pf3.py
@@ -26,24 +26,18 @@ dataset_file = "results/stage4/facilities-2004-2022.csv"
 facilities_data = pd.read_csv(dataset_file, encoding="utf-8-sig", low_memory=False)
 
 
-#Read consolidated facilities dataset
+
 
 #Read and apply the changes in the sectors list detected for the research team
 sectors_file = "management/sectors_list.csv"
 sectors_list = pd.read_csv(sectors_file, encoding="utf-8-sig", low_memory=False, sep="\t")
-#sectors_metadata.loc[:"industrialsectorname"] = ""
+
 facilities_data["industrialsectorname"] = ""
 sectors_metadatafile = "management/sectors_metadata.csv"
 sectors_metadata = pd.read_csv(sectors_metadatafile, encoding="utf-8-sig", low_memory=False)
 
-
-
 for index, row in sectors_list.iterrows():
     print(f"\t{row["industrialsectorname"]}")
-    
-    
-    #filtered_data = facilities_data[facilities_data["sector"] == row["dataset_industrialsectorname"], "management"] =
-    #filtered_data = sectors_metadata[sectors_metadata["industrialsectorname"] == row["industrialsectorname"]]
     
     
     if row["requires_update"] == 1:
@@ -54,14 +48,9 @@ for index, row in sectors_list.iterrows():
     
     filtered_data = sectors_metadata[sectors_metadata["industrialsectorname"] == row["industrialsectorname"]]#
     current_name = filtered_data["industrialsectorname_en"].iloc[0]
-    print(current_name)
-    facilities_data.loc[facilities_data["sector"] == row["industrialsectorname"], "industrialsectorname"] = current_name
+    
     #Add the sector name in English
+    facilities_data.loc[facilities_data["sector"] == row["industrialsectorname"], "industrialsectorname"] = current_name
     
-    
-  
-    
-    #facilities_data.loc[facilities_data[facilities_data["sector"]] == row["industrialsectorname"] ,"industrialsectorname"] = current_name
-
 facilities_data.to_csv("results/stage5/facilities-2004-2022.csv", encoding="utf-8-sig", index=False)
 
